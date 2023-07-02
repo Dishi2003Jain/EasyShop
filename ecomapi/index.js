3a7dcfc3f88e78cbce768e48e869ce6134464aba
Dishi <dishinayak223@gmail.com>
2023-07-02 12:07:02 +0530

    first commit

---
+++ ecomapi/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("Backend Server is running")
+})
