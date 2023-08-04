218802391bc723dc9147697c5024b67c645300da
Dishi <dishinayak223@gmail.com>
2023-08-04 15:47:50 +0530

    firs

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const connectDB = require("./config/db.js");
-const authRoutes = require("./routes/authRoutes.js");
-const categoryRoutes = require("./routes/categoryRoutes.js");
-const productRoutes = require("./routes/productRoutes.js");
-const cors = require("cors");
-const path = require('path');
-const { fileURLToPath } = require("url");
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from "url";
 dotenv.config();
 
 connectDB();
@@ -17,21 +17,24 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors());
+app.use(cors())
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.use("/api/v1/auth",authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
+
 app.use('*',function(req,res){
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+})
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`);
-});
+app.listen(PORT,()=>{
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`)
+})
+
+
